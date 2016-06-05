6361d8e03859ec8e457f60c601a6b6e921a8a512
MOB Admin <admin@demon.of.by>
2016-06-05 21:11:26 +0300

    GITHUB-13 - Fixed sh script. Added pre-defined git config user

--- vagrant/lib/provision-user.sh
+++ vagrant/lib/provision-user.sh
@@ -8,13 +8,16 @@ useradd -g dmitry -m -d /home/dmitry -c "Dmitry Vl. Rendov" -s /bin/bash dmitry
 mkdir /home/dmitry/.ssh
 chmod 700 /home/dmitry/.ssh
 
-echo /vagrant/ssh/id_rsa_dmitry_rendov.pub >> /home/dmitry/.ssh/authorized_keys
-chmod 640 /home/dmitry/.ssh/authorized_keys
-
-cat /vagrant/ssh/id_rsa_dmitry_rendov.pem > /home/dmitry/.ssh/id_rsa
-chmod 400 /home/dmitry/.ssh/id_rsa
-
+cat /vagrant/ssh/id_rsa_mobadmin.pub >> /home/dmitry/.ssh/authorized_keys
 cat /vagrant/ssh/config > /home/dmitry/.ssh/config
-chmod 640 /home/dmitry/.ssh/config
+chmod 640 /home/dmitry/.ssh/*
+
+cat /vagrant/ssh/gitconfig > /home/dmitry/.gitconfig
+chmod 640 /home/dmitry/.gitconfig
+
+cat /vagrant/ssh/id_rsa_dmitry_rendov.pem > /home/dmitry/.ssh/id_rsa_dmitry_rendov.pem
+cat /vagrant/ssh/id_rsa_mobadmin.pem > /home/dmitry/.ssh/id_rsa
+chmod 400 /home/dmitry/.ssh/id_rsa*
 
 chown -R dmitry:dmitry /home/dmitry/.ssh
+chown dmitry:dmitry /home/dmitry/.gitconfig
